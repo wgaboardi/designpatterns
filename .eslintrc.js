aced6b8751aa763febedaf75acee3f83cd26c406
Wellington Luiz Gaboardi <wgaboardi@gmail.com>
2022-06-17 20:45:34 -0300

    initial configs

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+//npm i -D eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser prettier eslint-config-prettier eslint-plugin-prettier
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-empty-function': 'off',
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+// extension coderunner instalado
+// extension es-lint
+// faz parecer typescript como linguagem interpretada ao invés de compilada
+console.log('aa');
 console.log('aa');
